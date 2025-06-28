eb871a6b7f31c33d1495b7a1966086c814d0eb87
ZYAD626A <dhemzyad403@gmail.com>
2025-06-28 16:21:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,152 @@
+let input = document.getElementById("input");
+let words = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum doloremque at possimus modi eaque. Obcaecati saepe voluptatum dolorum expedita, possimus rerum earum labore unde neque magnam ipsum praesentium laudantium, quos aperiam magni accusamus iste. Nulla nostrum temporibus dicta doloribus quibusdam vitae saepe nobis quae molestiae! Adipisci, maiores. Quod, exercitationem? Error pariatur, provident corrupti neque numquam ab expedita velit sapiente est tempore magni iste incidunt nobis amet voluptate. Architecto, alias. Asperiores, neque. Error magni dolorum ea quam odit neque voluptate ut nostrum ad doloribus magnam veniam, vitae reiciendis, natus rem impedit assumenda cupiditate corrupti non delectus. Ratione repellendus expedita laboriosam, et blanditiis distinctio? Voluptatibus corrupti quae reiciendis, earum porro nobis obcaecati optio itaque vel, recusandae tempore explicabo molestias debitis quisquam ab veritatis. Dolorum hic eum facere autem voluptates nobis! Dolorem consectetur porro quis delectus. Omnis, culpa amet. Molestias eos, mollitia fugit accusantium voluptatem delectus laudantium libero, sequi recusandae officia sed, quasi quo fugiat quisquam minus iste dignissimos expedita. At dolores natus sunt, fugit labore, dicta soluta ad quos alias necessitatibus rem sit rerum quasi beatae totam laboriosam? Explicabo, ut quos. Quas similique fugit incidunt enim ea quaerat facilis illum inventore ex earum dicta, dignissimos sint quo dolorum eligendi sit ullam, voluptates cumque sequi. Ad tempora aut ab ex quam ipsa beatae id eaque ducimus praesentium magnam fugiat quae at suscipit possimus facilis, ratione blanditiis vero dolore sed accusamus cumque accusantium voluptatem nesciunt! Minus vitae minima aliquam aperiam laborum modi eveniet a at accusantium dolore? Impedit eveniet tempore harum aspernatur. Soluta possimus suscipit, libero similique laborum non alias. Pariatur consequatur repellendus perspiciatis minima? Assumenda, saepe eum culpa, cupiditate possimus aliquid fugiat esse quidem delectus magnam est doloremque non dolore vel repellendus iure accusamus tempore perspiciatis, nulla dicta voluptate! Tenetur consequatur dolor, ea minima perferendis accusantium! Nemo deleniti placeat aut, suscipit dolores qui! Fuga explicabo pariatur earum reprehenderit nisi non similique, sapiente laboriosam magni velit. Ipsum harum explicabo, sequi blanditiis quas impedit vero voluptatibus ipsam, eum eius ducimus maiores dolore? Suscipit itaque perspiciatis error debitis saepe magni quae iste voluptas aliquid dolorem? Modi ratione dicta dolore incidunt sunt hic autem dolorum est consectetur, obcaecati accusantium, in harum, tenetur illum corporis. Distinctio ea, pariatur asperiores deserunt sequi modi quibusdam quos exercitationem, suscipit vel, excepturi ut eius ipsum alias ex possimus. Iure labore nesciunt cumque ratione saepe eligendi dolorem molestiae sunt facilis minima doloremque, est magnam at quisquam. Delectus mollitia a illo non ullam nam perspiciatis cum unde dolorem. Deleniti rem eius sapiente ducimus eveniet voluptatibus saepe incidunt inventore. Fugiat ducimus consequatur fuga minima laborum impedit voluptatem molestias mollitia quae, architecto cum similique dolore eaque reiciendis ex maxime natus omnis reprehenderit dolorem dolores eum iure? Impedit voluptatibus illum adipisci atque enim vero similique quas nam quo modi ratione, suscipit veritatis quibusdam nihil deserunt architecto officiis, autem praesentium nesciunt! Molestiae quas natus debitis adipisci cumque aliquid fugit quaerat quam, earum, ab in iusto ratione quasi distinctio. Reiciendis, laboriosam. Voluptatibus laudantium iure nostrum qui, eum suscipit, illum nemo autem quaerat praesentium inventore commodi debitis at, aspernatur distinctio nihil! Facere eum labore quae, non voluptatibus debitis sapiente est? Eligendi, nihil officia aspernatur dolore consectetur iusto nam vero fuga voluptatem, temporibus, ducimus excepturi quam dolor nostrum ea. Quod nesciunt cupiditate dolorum. Quasi amet officiis consequuntur assumenda. Optio, voluptatibus sequi amet facere, mollitia expedita non error dolores numquam laborum culpa exercitationem debitis sunt consectetur consequatur aut cupiditate ratione eaque atque. Officiis consequuntur eius quas explicabo facilis, voluptatum, eveniet ratione magnam quibusdam quidem fuga! Assumenda, voluptatum fugit non dolor accusantium vitae! Laboriosam debitis ducimus distinctio magni, quo aliquid, veniam, repellendus necessitatibus nostrum quaerat blanditiis repudiandae omnis eaque odio ea veritatis molestiae labore dolores iure cum. Id incidunt expedita harum commodi deleniti qui accusamus maxime voluptate officiis sunt consectetur aspernatur, cupiditate cum cumque veritatis, labore nostrum repudiandae dignissimos modi quas dolorum saepe adipisci pariatur? Impedit, incidunt non iusto provident ad, ratione modi deserunt alias sequi ab, tenetur et at debitis quasi dolor cum sapiente numquam itaque. Incidunt, voluptate molestias odio praesentium ex omnis molestiae deserunt. Non ea odio, esse dolorum qui repellendus totam veniam nemo nesciunt reprehenderit ducimus modi quia id praesentium sint veritatis? Modi maxime consequuntur, perferendis culpa, harum nulla nisi, autem necessitatibus explicabo tempore quae ducimus deserunt veniam assumenda ipsam magni consectetur ratione voluptatem possimus unde cum! Hic molestiae consequuntur maiores velit aperiam sequi. Officia libero possimus eius quidem sequi consequatur architecto quae dolorum dolore ab vero veniam, voluptatum eum delectus magnam esse nobis nihil! Sed officia impedit quam neque sequi nobis amet officiis, vitae voluptatem in dolorum minima dolor facilis doloremque cumque deserunt doloribus sapiente. Ad iste vero nostrum, ducimus, eos nobis suscipit itaque adipisci quaerat dolorum, delectus consequuntur soluta facere doloremque incidunt aliquam sit nesciunt inventore. Architecto qui voluptatibus laudantium, molestiae vero voluptas eum cumque. Delectus ea ducimus at culpa earum aliquid officiis voluptatibus, soluta ad cum similique nulla dolores vero provident illo nemo facilis eius, dolorum minus dolore laboriosam eum itaque est illum. Vero, beatae ducimus? Ad, ipsam sit maxime ducimus inventore iste, officiis ex voluptatum quod molestiae ullam nobis sunt quae sed reprehenderit magni fuga rerum culpa suscipit laudantium provident accusamus, distinctio cum sapiente! Quam odit perferendis temporibus nemo, beatae sed atque accusantium a asperiores numquam eum aut fugit. Porro, nobis maiores fugit voluptatem numquam in mollitia ratione error recusandae ipsa beatae modi nemo! Doloribus, iusto similique saepe beatae qui ratione dolores iure explicabo odit aperiam molestias, nam impedit fugit facere rerum commodi necessitatibus ea minima! In excepturi tempore velit dolor harum molestiae quidem ipsum adipisci! Facere, cum aspernatur fugiat, assumenda ipsam ex exercitationem est recusandae corporis optio, soluta repellat ab dolor sed voluptatum animi dicta id rerum? Eaque quibusdam deserunt exercitationem. Ex, nobis fuga perferendis excepturi, fugiat delectus obcaecati libero architecto ut possimus doloremque, exercitationem quae soluta a. Est consectetur, magni ratione debitis suscipit ducimus possimus cum dolores facilis temporibus, tenetur sit harum, saepe officia adipisci praesentium quibusdam? Doloribus, nihil atque possimus expedita exercitationem eaque at hic quia quaerat molestiae est nemo ratione labore, vitae praesentium officiis placeat. Maiores inventore in quaerat natus quis? Excepturi.";
+let signal = ['_','#','!','$'];
+let cleanWords = words.replace(/[.,!?]/g, "").split(" ");
+
+let typePassword = document.getElementById('strong')
+let lengthPass = document.getElementById('lengthPass')
+let SelectPassword = document.getElementById('selectTypePass')
+
+
+let div = document.querySelector(".resetButton");
+let btn = `<button onclick = "empty()">Reset</button>`;
+//add value locastorage in input
+
+
+
+
+function showPassword(btn){
+    if(btn.innerHTML === "Show"){
+        //add locastorage
+        localStorage.setItem('showPass',"Hide");
+        // ===========
+        input.type = "text";
+        btn.innerHTML = localStorage.getItem("showPass");
+        
+    }else{
+        //add locastorage
+        localStorage.setItem('showPass',"Show");
+        // ===========
+        input.type = "password";
+        btn.innerHTML = localStorage.getItem("showPass");
+    }
+    //add value localstorage in input
+}
+
+
+
+
+if(localStorage.length > 0) {
+    input.value = localStorage.getItem("date");
+}
+function runFunction(){
+       div.innerHTML = btn;
+   localStorage.setItem('date' , input.value)
+   
+   const w1 = Math.floor(Math.random() * cleanWords.length);
+   const w2 = Math.floor(Math.random() * cleanWords.length);
+   const w3 = Math.floor(Math.random() * signal.length);
+   let num = 1e6;
+   const randomNumbers = Math.floor(Math.random() * num) + 1;
+   let randomWords ;
+
+if(SelectPassword.value === "easy"){
+    num = 1e2;
+    randomWords=cleanWords[w1]
+    +(randomNumbers.toString())
+    +signal[w3];
+    //uppercase && lowercase
+    let resualt =  randomWords[1].toUpperCase() + randomWords ;
+    input.value = resualt;
+
+    lengthPass.innerHTML = input.value.length;
+
+}else if(SelectPassword.value === "normal"){
+    
+    num = 1e3;
+    randomWords=cleanWords[w1]
+    +cleanWords[w2]
+    +(randomNumbers.toString())
+    +signal[w3];
+    //uppercase && lowercase
+    let resualt =  randomWords[1].toUpperCase() + randomWords ;
+    input.value = resualt;
+
+    lengthPass.innerHTML = input.value.length;
+
+}else if(SelectPassword.value === "hard"){
+
+    num = 1e5;
+    randomWords=cleanWords[w1]
+    +cleanWords[w2]
+    +signal[w3]
+    +signal[w3]
+    +(randomNumbers.toString())+signal[w3];
+    
+    //uppercase && lowercase
+    let resualt =  randomWords[1].toUpperCase() + randomWords ;
+    input.value = resualt;
+
+
+    lengthPass.innerHTML = input.value.length;
+
+}else if(SelectPassword.value === "defecult"){
+    num = 1e9;
+     randomWords =  cleanWords[w1]
+    +signal[w3] 
+    +signal[w3]
+    + cleanWords[w1]
+    + cleanWords[w2]
+    + cleanWords[w2%2]
+    + randomNumbers.toString()+signal[w3];
+    //uppercase && lowercase
+    let resualt =  randomWords[1].toUpperCase() + randomWords ;
+    input.value = resualt;
+
+
+    lengthPass.innerHTML = input.value.length;
+
+}
+
+  
+}
+
+
+
+lengthPass.innerHTML = input.value.length;
+SelectPassword.onchange = function () {
+    typePassword.innerHTML = SelectPassword.value;
+}
+
+
+
+
+function copyText() {
+      // حدد العنصر اللي فيه النص
+let text = input.value;
+
+      // استخدم navigator.clipboard
+navigator.clipboard.writeText(text).then(() => {
+        alert("تم نسخ النص!");
+      }).catch(err => {
+        alert("حدث خطأ في النسخ 😢");
+        console.error(err);
+      });
+    }
+/*   const w2 = Math.floor(Math.random() * 7588) + 1;
+   const w3 = Math.floor(Math.random() * 7588) + 1;
+   const w4 = Math.floor(Math.random() * 7588) + 1;
+   const w5 = Math.floor(Math.random() * 7588) + 1;
+    */
+
+if(input.value.length > 0){
+    div.innerHTML = btn;
+}else{
+div.innerHTML = '';
+}
+ 
+function empty() {
+ input.value = "";
+ localStorage.date = ""
+ window.location.reload();
+}
